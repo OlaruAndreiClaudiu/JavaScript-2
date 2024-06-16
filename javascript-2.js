7932b7329d41c99adba3935246bc1ef320383016
Olaru Andrei Claudiu <olaru.andrei.claudiu@gmail.com>
2024-06-16 23:11:38 +0300

    Commit 5

--- javascript-2.js
+++ javascript-2.js
@@ -1,23 +1,26 @@
-
 function shop() {
   let stock = [
-    { name: "product 1", price: 15 },
-    { name: "product 2", price: 10 },
-    { name: "product 3", price: 20 },
-    { name: "product 4", price: 25 },
-    { name: "product 5", price: 17 },
-    { name: "product 6", price: 16 },
-    { name: "product 7", price: 16 },
+    { name: "product 1", price: 10 },
+    { name: "product 2", price: 20 },
+    { name: "product 3", price: 30 },
+    { name: "product 4", price: 40 },
+    { name: "product 5", price: 50 },
+    { name: "product 6", price: 60 },
+    { name: "product 7", price: 70 },
   ];
-  console.log(stock.length);
+  console.log(stock);
   console.log(`There are ${stock.length} products in stock.`);
   let cart = [];
+  let my_Cart = stock.concat(cart);
+  console.log(my_Cart);
+  my_Cart.push('product 8');
+  console.log(my_Cart);
   return function() {
-    cart.push(stock);
-    console.log(stock.length);
-    console.log(`You have ${stock.length} products in your cart.`);
-    cart.push(stock.length++);
-    console.log(`There are no more products to be added.`);
+    if(my_Cart.length > 7) {
+      console.log(`There are no more products to be added`);
+    } else {
+      console.log(`You have ${my_Cart.length} products in your cart.`);
+    }
   };
 }
 const shopping = shop();
