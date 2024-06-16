08dca86c02c1e3d5c4581075ccaffab75418c9a6
Olaru Andrei Claudiu <olaru.andrei.claudiu@gmail.com>
2024-06-16 23:37:48 +0300

    Commit 6

--- javascript-2.js
+++ javascript-2.js
@@ -13,11 +13,12 @@ function shop() {
   let cart = [];
   let my_Cart = stock.concat(cart);
   console.log(my_Cart);
+  console.log(`You have ${my_Cart.length} products in your cart.`)
   my_Cart.push('product 8');
   console.log(my_Cart);
   return function() {
     if(my_Cart.length > 7) {
-      console.log(`There are no more products to be added`);
+      console.log(`There are no more products to be added.`);
     } else {
       console.log(`You have ${my_Cart.length} products in your cart.`);
     }
